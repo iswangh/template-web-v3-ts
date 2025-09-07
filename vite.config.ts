dc364b409e75aa6c1abb0c1f1f5b1da36a68b614
王浩 <wangh.mail@qq.com>
2025-09-07 15:47:50 +0800

    chore:  集成 unplugin-auto-import/vite（自动导入），unplugin-vue-components/vite（自动注册组件）

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -10,6 +12,30 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    // 自动导入插件
+    AutoImport({
+      // 预设自动导入
+      imports: ['vue', 'vue-router', 'pinia'],
+      // 自定义自动导入
+      dirs: ['./src/apis', './src/composables', './src/stores', './src/utils'],
+      // 生成对应的 .d.ts 文件
+      dts: './src/types/auto-imports.d.ts',
+      // 解析器（如果使用 UI 组件库）
+      resolvers: [
+        // 示例：ElementPlusResolver()
+      ],
+    }),
+    // 自动注册组件
+    Components({
+      // 指定组件位置
+      dirs: ['src/components'],
+      // 生成对应的 .d.ts 文件
+      dts: './src/types/components.d.ts',
+      // 解析器（如果使用 UI 组件库）
+      resolvers: [
+        // 示例：ElementPlusResolver()
+      ],
+    }),
   ],
   resolve: {
     alias: {
