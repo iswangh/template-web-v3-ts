980f1a3722874cc36990da7a0c9d68565b13ff6c
王浩 <wangh.mail@qq.com>
2025-09-07 17:05:51 +0800

    refactor(vitest): 更新 Vitest 配置以支持 Vite 配置的参数传递
    - 将 Vitest 配置改为使用 defineConfig 函数
    - 为 Vite 配置添加 configEnv 参数
    - 调整 mergeConfig 的使用方式，合并 Vite 和 Vitest 配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,16 @@ import { fileURLToPath } from 'node:url'
 import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+export default defineConfig((configEnv) => {
+  return mergeConfig(
+    viteConfig(configEnv),
+    {
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url)),
+      },
     },
-  }),
+  )
+},
 )
