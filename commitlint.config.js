2390f6f6d3c6a44d64a8387586f6b26ab27c3510
王浩 <wangh.mail@qq.com>
2025-09-08 17:42:39 +0800

    chore:  美化 lint-staged husky 校验信息

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,14 +19,14 @@ export default {
       'always',
       [
         'feat', // 新功能
-        'fix', // 修复
-        'refactor', // 重构
-        'style', // 格式化和样式调整
+        'fix', // 修复 bug
+        'refactor', // 代码重构
+        'style', // 代码格式调整
         'perf', // 性能优化
         'test', // 测试相关
         'build', // 构建配置
         'ci', // CI/CD 配置
-        'docs', // 文档
+        'docs', // 文档更新
         'chore', // 日常维护
         'revert', // 回滚操作
       ],
