3546d0615e210de7af68bb8d955e59fbf89b87e4
wangh <wangh.mail@qq.com>
2025-09-28 13:40:20 +0800

    chore:  添加根目录的别名

--- vite.config.ts
+++ vite.config.ts
@@ -154,6 +154,7 @@ export default defineConfig(({ mode, command }) => {
     ].filter(Boolean),
     resolve: {
       alias: {
+        '~': fileURLToPath(new URL('./', import.meta.url)),
         '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
